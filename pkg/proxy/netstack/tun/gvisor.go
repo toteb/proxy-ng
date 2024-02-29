8ed6799da38ecc512b61ca75f9da00e75137150a
mcn1k <toteb@abv.bg>
2024-02-29 17:58:59 +0200

    Update gvisor.go

--- pkg/proxy/netstack/tun/gvisor.go
+++ pkg/proxy/netstack/tun/gvisor.go
@@ -4,10 +4,10 @@
 package tun
 
 import (
-	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/link/fdbased"
-	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/link/rawfile"
-	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/link/tun"
-	"github.com/nicocha30/gvisor-ligolo/pkg/tcpip/stack"
+	"github.com/toteb/gvisor/pkg/tcpip/link/fdbased"
+	"github.com/toteb/gvisor/pkg/tcpip/link/rawfile"
+	"github.com/toteb/gvisor/pkg/tcpip/link/tun"
+	"github.com/toteb/gvisor/pkg/tcpip/stack"
 )
 
 func Open(tunName string) (stack.LinkEndpoint, error) {
